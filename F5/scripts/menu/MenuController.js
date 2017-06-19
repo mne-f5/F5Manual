c1a3c8a36b2eef15196329bc6fd46b8b96db89b6
Negru <Negru@MIHAI>
2017-06-19 10:32:16 +0300

    Fixed login and menu issues

--- F5/scripts/menu/MenuController.js
+++ F5/scripts/menu/MenuController.js
@@ -6,34 +6,46 @@
     }
 
     $scope.goTo = function (path) {
+        var menu = $('.fill');
+        var body = $('body');
+
         $location.path(path);
         menuFactory.setActiveItem(path);
+
+        if (menu.hasClass('open')) {
+            menu.removeClass('open');
+            body.removeClass('no-overflow');
+        }
+                
     }
 
 
     $scope.showMenu = function () {
         var menu = $('.fill');
+        var body = $('body');
         if (menu.hasClass('open')) {
             menu.removeClass('open');
+            body.removeClass('no-overflow');
         }
         else {
             menu.addClass('open');
+            body.addClass('no-overflow');
         }
     }
 
-    $scope.toggleMenu = function () {
-        $scope.overlay = document.getElementsByClassName("main-wrapper")[0];
-        $scope.menu = document.getElementsByClassName("navigation-menu")[0];
+    //$scope.toggleMenu = function () {
+    //    $scope.overlay = document.getElementsByClassName("main-wrapper")[0];
+    //    $scope.menu = document.getElementsByClassName("navigation-menu")[0];
 
-        if ($scope.menu.classList.contains("opened")) {
-            $scope.menu.classList.remove("opened");
-            $scope.overlay.classList.remove("with-overlay");
-        }
-        else {
-            $scope.menu.className += " opened";
-            $scope.overlay.className += " with-overlay";
-        }
+    //    if ($scope.menu.classList.contains("opened")) {
+    //        $scope.menu.classList.remove("opened");
+    //        $scope.overlay.classList.remove("with-overlay");
+    //    }
+    //    else {
+    //        $scope.menu.className += " opened";
+    //        $scope.overlay.className += " with-overlay";
+    //    }
 
-    };
+    //};
 
 });
